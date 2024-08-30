50798acdd8f5d0c5df38cb66680a5ef9eab2f65d
sange5 <163669819+sange5@users.noreply.github.com>
2024-08-30 15:08:06 +0300

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,6 +5,3 @@ from .serializers import BookSerializer
 class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-from rest_framework import viewsets
-from .models import Book
-from .serializers import BookSerializer
