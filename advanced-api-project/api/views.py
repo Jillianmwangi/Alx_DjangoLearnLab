4ed816945ec56aeca0e6e4daabeea2ee6425f687
sange5 <163669819+sange5@users.noreply.github.com>
2024-09-06 15:22:38 +0300

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -156,6 +156,21 @@ class BookListView(generics.ListAPIView):
     
     ordering = ['title']
 
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from .models import Book
+from .serializers import BookSerializer
+
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]  
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  
+
 
     
 
