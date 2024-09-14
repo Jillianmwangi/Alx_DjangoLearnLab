1ac449d488197838d3be3274ff6ab5a0d3339be8
sange5 <163669819+sange5@users.noreply.github.com>
2024-09-14 12:39:34 +0300

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -181,3 +181,32 @@ def search_posts(request):
         ).distinct()  # Avoid duplicate posts in results
 
     return render(request, 'blog/search_results.html', {'posts': posts, 'query': query})
+
+from django.urls import path
+from .views import PostByTagListView
+
+urlpatterns = [
+    # Other URL patterns
+    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='posts_by_tag'),
+]
+
+from django.views.generic import ListView
+from .models import Post
+from taggit.models import Tag
+
+class PostByTagListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        tag_slug = self.kwargs.get('tag_slug')
+        if tag_slug:
+            tag = Tag.objects.get(slug=tag_slug)
+            return Post.objects.filter(tags__in=[tag])
+        return Post.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tag_slug'] = self.kwargs.get('tag_slug')
+        return context
