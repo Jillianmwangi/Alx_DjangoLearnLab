b756f36c50c0365f33155c2f5dfa6ae07082f19f
sange5 <163669819+sange5@users.noreply.github.com>
2024-09-22 17:13:34 +0300

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -82,6 +82,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'your_db_host',  
+        'PORT': '5432'
     }
 }
 
